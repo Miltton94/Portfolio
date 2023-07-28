14b1c26b2d13e90d1f5a9af02687485283bb771b
Milton <miltonsilva126@gmail.com>
2023-07-28 08:11:27 -0300

    feat: add tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import ContactForm from '@/components/Contact-form'
 import Footer from '@/components/Footer'
 import BackToTop from '@/components/Back-to-top'
 import Toaster from '@/components/Toaster'
+import Script from 'next/script'
 
 const inter = Inter({
   variable: '--font-inter',
@@ -56,7 +57,25 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR" className={`${inter.variable} ${plexMono.variable}`}>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${process.env.GTM_ID}');
+        `}
+      </Script>
+
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Toaster />
         <BackToTop />
 
